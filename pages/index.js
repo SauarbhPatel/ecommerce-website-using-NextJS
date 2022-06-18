f91aec16441c8da599e712adcc2a259b1cbf32ab
Saurabh Patel <saurabh123815@gmail.com>
2022-06-18 15:13:34 +0530

    start

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>CodesWere.com - Wear the code</title>
+        <meta name="description" content="CodesWere.com - Wear to code" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+    hii thi 
+    <div className="mx-4 bg-slate-500">hello</div>
+    </div>
+  )
+}
